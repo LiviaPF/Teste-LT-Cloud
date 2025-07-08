0cafadbd6a2bd594573bb34b47a15b876c069a03
livia <liviap520@gmail.com>
2025-07-07 21:13:08 -0300

    style: alterações no estilo da página e adição do link para visualizar artigo individualmente

--- resources/views/livewire/articles.blade.php
+++ resources/views/livewire/articles.blade.php
@@ -1,81 +1,82 @@
-<div class="mb-6 w-full">
-    <div class="flex justify-between items-center mb-2">
-        <flux:heading size="xl" level="1">{{ __('Articles') }}</flux:heading>
-        <div class="flex items-center justify-end gap-2">
-            <flux:input class="mt-4" wire:model.live="searchQuery" placeholder="Article title..." />
-            <a href="{{ route('articles.create') }}" target="_blank">
-                <flux:button class="mt-4">Add article</flux:button>
-            </a>
+<div class="relative mb-6 w-full">
+    <div class="flex flex-col sm:flex-row sm:items-center justify-between mb-2 gap-4">
+        <flux:heading size="xl" level="1" class="flex-shrink-0 text-center w-full sm:w-auto">{{ __('Articles') }}</flux:heading>
+        <div class="flex items-center justify-start gap-2 w-full sm:flex-1">
+            <flux:input class="w-full sm:flex-1" wire:model.live="searchQuery" placeholder="Article title..." />
+            <flux:button class="flex-shrink-0" wire:navigate href="{{ route('articles.create') }}">Add article</flux:button>
         </div>
     </div>
     <flux:separator variant="subtle" class="mb-4" />
 
-    @if (session('success'))
-        <div
-            x-data="{ show: true }"
-            x-show="show"
-            x-init="setTimeout(() => show = false, 3000)"
-            class="fixed bottom-5 right-5 bg-green-500 text-white text-sm p-4 rounded-lg shadow-lg z-50"
-            role="alert"
-        >
-            <p>{{ session('success') }}</p>
-        </div>
-    @endif
+    @session('success')
+    <div
+        x-data="{ show: true }"
+        x-show="show"
+        x-init="setTimeout(() => show = false, 3000)"
+        class="absolute top-5 right-5 bg-green-500 text-zinc-500 text-sm p-4 rounded-lg shadow-lg z-50"
+        role="alert"
+    >
+        <p>{{ $value }}</p>
+    </div>
+    @endsession('success')
 
-    <div class="grid gap-4 grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
+    <div class="grid gap-4 grid-cols-1">
         @forelse ($articles as $article)
-            <div class="rounded-lg shadow p-4 border border-white">
+            <div class="rounded-lg shadow p-4 border border-white max-w-full max-h-screen overflow-hidden">
                 <div class="flex flex-col gap-4">
                     <div>
                         <div class="flex items-start justify-between mb-2 gap-3">
                             <div class="block mb-2 gap-3">
-                                <span class="text-lg font-semibold flex text-gray-800">{{ $article->title }}</span>
+                                <span class="text-lg font-semibold flex">{{ $article->title }}</span>
                                 <flux:badge size="sm" class="h-6 min-w-[28px] flex items-center justify-center">
-                                {{ $article->developers->count() }} {{ Str::plural('author', $article->developers->count()) }}
+                                    {{ $article->developers->count() }} {{ $article->developers->count() === 1 ? 'developer' : 'developers' }}
                                 </flux:badge>
                             </div>
-                            <div class="flex items-start gap-1">
-                                <a href="{{ route('articles.edit', $article->id) }}" target="_blank">
-                                    <flux:button  size="xs" variant="outline"><flux:icon.pencil-square variant="mini" /></flux:button>
-                                </a>
+                            <div class="flex items-center gap-1">
+                                <flux:button size="xs" variant="outline" wire:navigate href="{{ route('articles.edit', ['id' => $article->id]) }}"><flux:icon.pencil-square variant="mini" /></flux:button>
                                 <flux:button size="xs" variant="danger" wire:click="delete({{ $article->id }})"><flux:icon.x-mark variant="mini" /></flux:button>
                             </div>
                         </div>
-                        <div class="text-gray-500 text-sm flex items-stretch gap-1">
-                            <flux:icon.calendar-days variant="mini"/>
-                            {{ $article->created_at->format('d/m/Y') }}
-                        </div>
+                        <p class="text-gray-500 line-clamp-3">{{ $article->content }}</p>
+                    </div>
+                    <div class="text-gray-500 text-sm flex items-stretch gap-1">
+                        <flux:icon.calendar-days variant="mini"/>
+                        {{ $article->created_at->format('d/m/Y') }}
+                    </div>
+                    <div class="flex justify-end">
+                        <a href="{{ route('articles.show', ['slug' => $article->slug]) }}" class="hover:underline text-sm">Read full article</a>
                     </div>
                 </div>
             </div>
         @empty
-            <div class="col-span-4 text-gray-500 text-center">
+            <div class="col-span-4 text-gray-500">
                 <p>{{ __('No articles found.') }}</p>
             </div>
         @endforelse
     </div>
 
     <div class="mt-4">
-        {{ $articles->links() }}
+        {{ $articles->links('') }}
     </div>
 
-    @if ($articleId)
-        <flux:modal name="delete-article-{{ $articleId }}" class="min-w-[22rem] bg-white rounded-lg p-6">
+    @foreach ($articles as $article)
+        <flux:modal name="delete-article-{{ $article->id }}" class="min-w-[22rem]">
             <div class="space-y-6">
                 <div>
-                    <flux:heading size="lg" class="text-xl font-semibold text-gray-800">Delete article?</flux:heading>
-                    <flux:text class="mt-2 text-gray-600">
+                    <flux:heading size="lg">Delete article?</flux:heading>
+                    <flux:text class="mt-2">
                         <p>You're about to delete this article.</p>
                         <p>This action cannot be reversed.</p>
                     </flux:text>
                 </div>
-                <div class="flex gap-2 justify-end">
+                <div class="flex gap-2">
+                    <flux:spacer />
                     <flux:modal.close>
-                        <flux:button variant="ghost" class="text-gray-600 hover:bg-gray-100">Cancel</flux:button>
+                        <flux:button variant="ghost">Cancel</flux:button>
                     </flux:modal.close>
-                    <flux:button variant="danger" class="bg-red-500 text-white hover:bg-red-600" wire:click="deleteArticle">Delete article</flux:button>
+                    <flux:button type="submit" variant="danger" wire:click="deleteArticle">Delete article</flux:button>
                 </div>
             </div>
         </flux:modal>
-    @endif
+    @endforeach
 </div>
